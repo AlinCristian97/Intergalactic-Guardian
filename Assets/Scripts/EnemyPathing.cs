baeb5271f93bc664eb5f66bea45f2f586cd2b972
Alin Cristian <alin.cristian977@gmail.com>
2021-06-04 22:05:03 +0200

    Created first Path prefab with its respective Waypoints

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] private List<Transform> _waypoints;
+    
+
+}
