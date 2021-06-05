5e50bc303e88812e2315a51a1ed3d0e4d45cfc87
Alin Cristian <alin.cristian977@gmail.com>
2021-06-05 20:39:00 +0200

    Implemented Shooting functionality for the Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,11 +2,44 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float _health = 100f;
+    
+    [SerializeField] private GameObject _projectile;
+    [SerializeField] private float _shotCounter;
+    [SerializeField] private float _minTimeBetweenShots = 0.2f;
+    [SerializeField] private float _maxTimeBetweenShots = 3f;
+    [SerializeField] private float _projectileSpeed = 10f;
 
+    private void Start()
+    {
+        _shotCounter = Random.Range(_minTimeBetweenShots, _maxTimeBetweenShots);
+    }
+
+    private void Update()
+    {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        _shotCounter -= Time.deltaTime;
+        if (_shotCounter <= 0f)
+        {
+            Fire();
+            _shotCounter = Random.Range(_minTimeBetweenShots, _maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(_projectile, transform.position, Quaternion.identity);
+
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -_projectileSpeed);
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
